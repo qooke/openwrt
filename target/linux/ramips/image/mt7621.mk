8d7e4f793464e7c5779bbd8d169382a8a8ed2582
qooke <83923036@qq.com>
2026-04-22 10:31:24 +0900

    ramips: add support for MelonPi M21
    Add support for the MelonPi M21, a compact dual-Gigabit Ethernet router
    based on the MediaTek MT7621AT SoC.
    
    Specifications:
    
    - SoC: MediaTek MT7621AT (MIPS 1004Kc @ 880 MHz)
    - RAM: 128 MiB / 512 MiB DDR3
    - Flash: 16 MiB / 32 MiB SPI-NOR
    - Ethernet:
      - 2x 10/100/1000 Mbps ports (1x WAN, 1x LAN)
      - MediaTek MT7530 switch (integrated)
    - USB: 1x USB 3.0 Type-A
    - Storage: 1x MicroSD (SDXC) slot
    - Buttons:
      - Reset button (GPIO#18)
      - User button (GPIO#13)
    - LEDs:
      - Red Power LED
      - Blue System LED (GPIO#16)
      - Green User LED (GPIO#0)
    - UART:
      - 4-pin header (2.54 mm pitch, J1, not populated)
      - 115200 8N1
      - Pinout: 3V3, TX, RX, GND
    - Power: Type-C 5V/2A with ch340
    
    Installation:
    
    The vendor firmware is based on OpenWrt. The generated sysupgrade image
    can be flashed from the vendor firmware using:
    
    sysupgrade -F <image>
    
    Do not preserve settings when flashing from vendor firmware.
    
    Recovery installation through U-Boot:
    
    1. Hold the reset button during boot for 5 seconds.
    2. Access the recovery web interface at 192.168.1.1.
    3. Upload the OpenWrt image.
    
    Signed-off-by: qooke <83923036@qq.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2196,6 +2196,32 @@ define Device/linksys_re7000
 endef
 TARGET_DEVICES += linksys_re7000
 
+define Device/melonpi-m21-16m
+  $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)  
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := MelonPi
+  DEVICE_MODEL := M21
+  DEVICE_VARIANT := 16M
+  DEVICE_PACKAGES := kmod-mmc-mtk kmod-usb3 kmod-usb-ledtrig-usbport \
+  kmod-usb-net-qmi-wwan kmod-usb-serial-option kmod-usb-net-rndis \
+  -wpad-basic-mbedtls -uboot-envtools
+endef
+TARGET_DEVICES += melonpi-m21-16m
+
+define Device/melonpi-m21-32m
+  $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
+  IMAGE_SIZE := 32448k
+  DEVICE_VENDOR := MelonPi
+  DEVICE_MODEL := M21
+  DEVICE_VARIANT := 32M
+  DEVICE_PACKAGES := kmod-mmc-mtk kmod-usb3 kmod-usb-ledtrig-usbport \
+  kmod-usb-net-qmi-wwan kmod-usb-serial-option kmod-usb-net-rndis \
+  -wpad-basic-mbedtls -uboot-envtools
+endef
+TARGET_DEVICES += melonpi-m21-32m
+
 define Device/maginon_mc-1200ac
   $(Device/dsa-migration)
   DEVICE_VENDOR := Maginon
